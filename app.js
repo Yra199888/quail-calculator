d033a27b38346d596fd3ae19b4cef0203470dfc4
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-09 17:16:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { loadLocal } from "./core/storage.js";
 import { renderAll } from "./modules/render.js";
+import { initDrive } from "./core/drive.js";
 
+initDrive();
 loadLocal();
 renderAll();
