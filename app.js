e71c96a61880924c5cf229b93208a8cfe98ebe6d
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 18:54:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 import { loadAppState } from "./state/state.load.js";
 
 loadAppState();
+
+
+import { saveAppState } from "../state/state.save.js";
+
+AppState.feedCalculator.volume = 25;
+saveAppState();
