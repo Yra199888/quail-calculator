ff426b789cade6bcb8bbf62a2393621462aa847b
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 19:00:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ import { saveAppState } from "../state/state.save.js";
 
 AppState.feedCalculator.volume = 25;
 saveAppState();
+
+import { $, qs, qsa } from "../utils/dom.js";
+
+const btn = $("makeFeedBtn");
+const rows = qsa(".qty");
+
+import { uid } from "../utils/uid.js";
+
+const id = uid("recipe_");
+
+import { required } from "../utils/validators.js";
+
+if (!required(client)) {
+  toast("Вкажи клієнта", "warn");
+}
