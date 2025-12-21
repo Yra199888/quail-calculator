625ea36654fa8931442bcf9e86a164153df956b1
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 18:58:22 +0200

    Update dom.js

--- utils/dom.js
+++ utils/dom.js
@@ -1,1 +1,8 @@
-w
+// src/utils/dom.js
+
+export const $ = (id) => document.getElementById(id);
+
+export const qs = (sel, root = document) => root.querySelector(sel);
+
+export const qsa = (sel, root = document) =>
+  Array.from(root.querySelectorAll(sel));
