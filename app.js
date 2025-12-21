330cd2c21b827fae7613a5d31dc7c6eaeb69b731
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 21:44:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,18 @@ import { required } from "../utils/validators.js";
 if (!required(client)) {
   toast("Вкажи клієнта", "warn");
 }
+
+import { FeedFormController } from "./controllers/FeedFormController.js";
+import { calculateFeed } from "./services/feed.service.js";
+import { renderFeedTotals } from "./render/feed.render.js";
+
+const feedForm = new FeedFormController({
+  AppState,
+  onChange: () => {
+    const result = calculateFeed(AppState);
+    renderFeedTotals(AppState);
+    saveAppState();
+  }
+});
+
+feedForm.init();
