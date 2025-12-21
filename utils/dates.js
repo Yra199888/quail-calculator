3dc16374743450b4b9438cc2bc5ed00417bc1cc7
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 19:41:01 +0200

    Update dates.js

--- utils/dates.js
+++ utils/dates.js
@@ -1,14 +1,9 @@
-// src/utils/dates.js
+// utils/dates.js
 
 export function isoToday() {
   return new Date().toISOString().slice(0, 10);
 }
 
-export function sortDatesAsc(dates) {
-  return dates.slice().sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
-}
-
-export function formatDateTime(iso) {
-  const d = new Date(iso);
-  return isNaN(d.getTime()) ? iso : d.toLocaleString();
+export function sortDatesAsc(dates = []) {
+  return [...dates].sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
 }
