b810110e8471383d90c3afaa417be7f350aa6d98
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-21 18:58:53 +0200

    Update uid.js

--- utils/uid.js
+++ utils/uid.js
@@ -1,1 +1,10 @@
-w
+// src/utils/uid.js
+
+export function uid(prefix = "") {
+  return (
+    prefix +
+    Date.now().toString(36) +
+    "_" +
+    Math.random().toString(36).slice(2, 8)
+  );
+}
