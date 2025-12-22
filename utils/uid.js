39e61a23b980c6c7eaa6f6039077116122760725
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2025-12-22 11:21:32 +0200

    Update uid.js

--- utils/uid.js
+++ utils/uid.js
@@ -1,5 +1,32 @@
-// utils/uid.js
+/**
+ * utils/uid.js
+ * ---------------------------------------
+ * Генерація унікальних ідентифікаторів.
+ * Використовується для:
+ *  - записів яєць
+ *  - замовлень
+ *  - рецептів
+ *  - історії замісів
+ */
 
-export function uid(prefix = "id") {
-  return `${prefix}_${Date.now()}_${Math.random().toString(16).slice(2)}`;
+/**
+ * Генерує унікальний ID
+ * Формат: <timestamp>_<random>
+ * Приклад: 1712345678901_a3f9c1
+ * @returns {string}
+ */
+export function uid() {
+  return (
+    Date.now().toString(36) +
+    "_" +
+    Math.random().toString(36).slice(2, 8)
+  );
+}
+
+/**
+ * Генерує короткий ID (для UI, якщо потрібно)
+ * @returns {string}
+ */
+export function shortUid() {
+  return Math.random().toString(36).slice(2, 7);
 }
