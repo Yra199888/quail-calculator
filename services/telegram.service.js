27ac3babc5e9bb5a92536f4ea2c9b38fee349f85
Yra199888 <152343374+Yra199888@users.noreply.github.com>
2026-01-04 12:32:21 +0200

    Update telegram.service.js

--- services/telegram.service.js
+++ services/telegram.service.js
@@ -1,13 +1,13 @@
 // services/telegram.service.js
 export function sendTelegram(text) {
   const TOKEN = "8587753988:AAED18mOkUVo3TniDRnU0pCLNT-5UzR7cdQ";
-  const CHAT_ID = "8587753988";
+  const CHAT_ID = "6182525216";
 
   fetch(`https://api.telegram.org/bot8587753988:AAED18mOkUVo3TniDRnU0pCLNT-5UzR7cdQ/sendMessage`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      chat_id: 8587753988,
+      chat_id: 6182525216,
       text,
       parse_mode: "HTML"
     })
